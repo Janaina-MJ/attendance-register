4a250084c62ae888de5da19be55a326e450a21f4
Janaina-MJ <janaina.marostegadejesus@gmail.com>
2022-07-04 19:23:27 +0100

    feat: Add basic style to the homepage

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,12 +1,12 @@
-function Home() {
+import './styles.css';
+
+export function Home() {
   return (
-    <>
+    <div className="container">
        <h1>Attendance Register</h1>
        <input type="text" placeholder="Enter your name" />
        <button>Register</button>
-    </>
+    </div>
    
   )
 }
-
-export default Home
