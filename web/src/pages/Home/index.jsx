91abf8b80a5e59ea47dafa11aa1a3547372caad3
Janaina-MJ <janaina.marostegadejesus@gmail.com>
2022-07-05 14:03:09 +0100

    feat: Add Card component to display names and time of each register

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,11 +1,15 @@
 import './styles.css';
 
+import { Card } from '../../components/Card'
+
 export function Home() {
   return (
     <div className="container">
        <h1>Attendance Register</h1>
        <input type="text" placeholder="Enter your name" />
        <button>Register</button>
+
+       <Card />
     </div>
    
   )
